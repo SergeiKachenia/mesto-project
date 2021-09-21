37b7477b8cf9e3cd296e79c0c0126e7b01af3d41
Sergei Kachenia <sergeikachenya@yandex.ru>
2021-09-21 17:57:17 +0300

    изменения + добавление функциональности открытия фото

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
+// переменные, которые использую несколько раз
+
 const content = document.querySelector('.content');
 const closeButtons = content.querySelectorAll('.popup__close-button');
 const popupForms = content.querySelectorAll('.popup__form');
 const popups = content.querySelectorAll('.popup');
 const nameInput = content.querySelectorAll('.popup__field_content_name');
 const descInput = content.querySelectorAll('.popup__field_content_description');
+const elements = document.querySelector('.elements');
 
+// массив с карточками
 
 const initialCards = [{
         name: 'Архыз',
@@ -31,22 +35,55 @@ const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+// функция лайка карточки
 
+function likeCard(evt) {
+    evt.target.classList.toggle('element__like-button_active');
+}
+// функция удаления карточки
 
-initialCards.forEach(function addInitialCards(cardItem) {
+function deleteCard(evt) {
+    evt.target.closest('.element').remove();
+}
+// функция закрытия попапа
+
+function closePopup() {
+    document.querySelector('.popup_opened').classList.remove('popup_opened');
+}
+// функция открытия попапа с увеличенным фото
+
+function openPlacePopup(placeSrc, placeCaption) {
+    const popupPlace = document.querySelector('.popup_type_photo');
+    popupPlace.querySelector('.popup__image').src = placeSrc;
+    popupPlace.querySelector('.popup__image').alt = placeCaption;
+    popupPlace.querySelector('.popup__caption').textContent = placeCaption;
+    popupPlace.classList.add('popup_opened');
+}
+
+// слушатель на фото в карточке для открытия попапа с увеличенными фото
+
+elements.addEventListener('click', function(evt) {
+    const target = evt.target
+    if (target.closest('.element__image')) {
+        openPlacePopup(target.src, target.alt);
+    }
+});
+
+//функция добавления начальных карточек
+
+initialCards.forEach((cardItem) => {
     const cardsTemplate = document.querySelector('.cards-template').content;
     const cardsElement = cardsTemplate.querySelector('.element').cloneNode(true);
-    const elements = document.querySelector('.elements');
     cardsElement.querySelector('.element__image').src = cardItem.link;
+    cardsElement.querySelector('.element__image').alt = cardItem.name;
     cardsElement.querySelector('.element__title').textContent = cardItem.name;
     elements.append(cardsElement);
-    cardsElement.querySelector('.element__like-button').addEventListener('click', function likeCard(evt) {
-        evt.target.classList.toggle('element__like-button_active');
-    });
-    cardsElement.querySelector('.element__delete-button').addEventListener('click', function deleteCard() {
-        cardsElement.remove();
-    });
+    cardsElement.querySelector('.element__like-button').addEventListener('click', likeCard);
+    cardsElement.querySelector('.element__delete-button').addEventListener('click', deleteCard);
 });
+
+
+// два слушателя на разные кнопки, которые открывают разные попапы
 
 content.querySelector('.profile__edit-button').addEventListener('click', () => {
     popups[0].classList.add('popup_opened');
@@ -56,17 +93,13 @@ content.querySelector('.profile__add-button').addEventListener('click', () => {
     popups[1].classList.add('popup_opened');
 });
 
-
+// слушатель на кнопки закрытия (крестики) через цикл for
 
 for (let i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', () => {
-        popups[i].classList.remove('popup_opened');
-        popupForms.forEach(function resetForm(formItem) {
-            formItem.reset();
-        });
-    })
+    closeButtons[i].addEventListener('click', closePopup)
 };
 
+// функция кнопки сабмит первого попава (редактирования профиля)
 
 popupForms[0].addEventListener('submit', (event) => {
     event.preventDefault();
@@ -76,9 +109,10 @@ popupForms[0].addEventListener('submit', (event) => {
     descInput[0].textContent = descInput[0].value;
     profileName.textContent = nameInput[0].value;
     profileDesc.textContent = descInput[0].value;
-    popups[0].classList.remove('popup_opened');
-    popupForms[0].reset();
+    closePopup();
 });
+
+// функция кнопки сабмит второго попапа (добавления места)
 
 popupForms[1].addEventListener('submit', (event) => {
     event.preventDefault();
@@ -86,14 +120,11 @@ popupForms[1].addEventListener('submit', (event) => {
     const cardsElement = cardsTemplate.querySelector('.element').cloneNode(true);
     const elements = document.querySelector('.elements');
     cardsElement.querySelector('.element__image').src = descInput[1].value;
+    cardsElement.querySelector('.element__image').alt = nameInput[1].value;
     cardsElement.querySelector('.element__title').textContent = nameInput[1].value;
     elements.prepend(cardsElement);
-    cardsElement.querySelector('.element__like-button').addEventListener('click', function likeCard(evt) {
-        evt.target.classList.toggle('element__like-button_active');
-    });
-    cardsElement.querySelector('.element__delete-button').addEventListener('click', function deleteCard() {
-        cardsElement.remove();
-    });
-    popups[1].classList.remove('popup_opened');
+    cardsElement.querySelector('.element__like-button').addEventListener('click', likeCard);
+    cardsElement.querySelector('.element__delete-button').addEventListener('click', deleteCard);
+    closePopup();
     popupForms[1].reset();
 });
